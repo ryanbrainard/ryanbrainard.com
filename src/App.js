315baeec4a6da13d6dfc44d2ff2d3022534847fb
Ryan Brainard <brainard@heroku.com>
2017-11-09 10:14:33 +0900

    Celedon

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <a href="/" style={{ textDecoration: "none" }}>
           <h1>ryanbrainard.com</h1>
         </a>
+
         <nav>
           <ul>
             <HeadlineIcon
@@ -28,102 +29,96 @@ class App extends Component {
             />
           </ul>
         </nav>
-        <div>
-          <div>
-            <h2>Projects</h2>
-            <ul>
-              <Listing
-                name="YouTube to Readlang"
-                href="http://youtube2readlang.ryanbrainard.com"
-                code="https://github.com/ryanbrainard/youtube2readlang"
-                intro="Search for a video and import into Readlang with synchronized subtitles"
-              />
-              <Listing
-                name="How Many Korean Words Do You Know?"
-                href="http://howmanykoreanwordsdoyouknow.com"
-                code="https://github.com/ryanbrainard/korean-vocabulary-quiz"
-                intro="Calculate the approximate number of words a Korean learner knows"
-              />
-              <Listing
-                name="jjogaegi"
-                href="http:/jjogaegi.ryanbrainard.com"
-                code="https://github.com/ryanbrainard/jjogaegi"
-                intro="Korean Vocabulary Parser-Formatter"
-              />
-              <Listing
-                name="Axiom"
-                href="https://axiomsso.herokuapp.com"
-                code="https://github.com/ryanbrainard/axiom"
-                intro="Tools for learning, testing, and troubleshooting single sign-on solutions for Salesforce.com"
-              />
-              <Listing
-                name="Workbench"
-                href="https://workbench.developerforce.com"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="Web-based suite of tools for administrators and developers to interact with the Force.com APIs"
-              />
-            </ul>
-          </div>
-          <div>
-            <h2>Writings</h2>
-            <ul>
-              <Listing
-                name="Korean as a Concatenative, Stack-Oriented Language"
-                href="http://post.naver.com/viewer/postView.nhn?volumeNo=8912179&memberNo=33582594"
-                intro="Musings about viewing Korean through the lens of a concatenative, stack-Oriented language"
-                date="2017-08-01"
-              />
-              <Listing
-                name="Korean-Korean Flashcards"
-                href="http://post.naver.com/viewer/postView.nhn?volumeNo=8292571&memberNo=33582594"
-                intro="The benefits of using monolingual flashcards"
-                date="2017-06-24"
-              />
-              <Listing
-                name="Introducing React Refetch"
-                href="https://blog.heroku.com/react-refetch"
-                code="https://github.com/heroku/react-refetch"
-                intro="A simple, declarative, and composable way to fetch data for React components"
-                date="2015-12-15"
-              />
-              <Listing
-                name="Bulk API Queries"
-                href="https://developer.salesforce.com/blogs/engineering/2011/03/bulk-api-queries.html"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="Bulk query support in Workbench 3.0"
-                date="2011-03-21"
-              />
-              <Listing
-                name="Workbench for Winter '11 Now Available"
-                href="https://developer.salesforce.com/blogs/engineering/2010/10/workbench-for-winter-11-now-available.html"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="Workbench enhancements for Winter '11 Release"
-                date="2010-10-17"
-              />
-              <Listing
-                name="Workbench 3.0 with Metadata API Support and More!"
-                href="https://developer.salesforce.com/blogs/engineering/2010/06/workbench-30-with-metadata-api-support-and-more.html"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="Enhancement for Workbench 3.0"
-                date="2010-06-20"
-              />
-              <Listing
-                name="PHP Client for Force.com Bulk API"
-                href="https://developer.salesforce.com/blogs/engineering/2009/11/php-client-for-forcecom-bulk-api.html"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="Introduction of PHP client for Bulk API"
-                date="2009-11-07"
-              />
-              <Listing
-                name="Workbench: From Idea to 2.0"
-                href="https://developer.salesforce.com/blogs/engineering/2008/07/workbench-from.html"
-                code="https://github.com/ryanbrainard/forceworkbench"
-                intro="How Workbench went from a modest idea to shipping v2.0"
-                date="2008-07-01"
-              />
-            </ul>
-          </div>
-        </div>
+
+        <ListingGroup name="Projects">
+          <Listing
+            name="YouTube to Readlang"
+            href="http://youtube2readlang.ryanbrainard.com"
+            code="https://github.com/ryanbrainard/youtube2readlang"
+            intro="Search for a video and import into Readlang with synchronized subtitles"
+          />
+          <Listing
+            name="How Many Korean Words Do You Know?"
+            href="http://howmanykoreanwordsdoyouknow.com"
+            code="https://github.com/ryanbrainard/korean-vocabulary-quiz"
+            intro="Calculate the approximate number of words a Korean learner knows"
+          />
+          <Listing
+            name="jjogaegi"
+            href="http:/jjogaegi.ryanbrainard.com"
+            code="https://github.com/ryanbrainard/jjogaegi"
+            intro="Korean Vocabulary Parser-Formatter"
+          />
+          <Listing
+            name="Axiom"
+            href="https://axiomsso.herokuapp.com"
+            code="https://github.com/ryanbrainard/axiom"
+            intro="Tools for learning, testing, and troubleshooting single sign-on solutions for Salesforce.com"
+          />
+          <Listing
+            name="Workbench"
+            href="https://workbench.developerforce.com"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="Web-based suite of tools for administrators and developers to interact with the Force.com APIs"
+          />
+        </ListingGroup>
+
+        <ListingGroup name="Writings">
+          <Listing
+            name="Korean as a Concatenative, Stack-Oriented Language"
+            href="http://post.naver.com/viewer/postView.nhn?volumeNo=8912179&memberNo=33582594"
+            intro="Musings about viewing Korean through the lens of a concatenative, stack-Oriented language"
+            date="2017-08-01"
+          />
+          <Listing
+            name="Korean-Korean Flashcards"
+            href="http://post.naver.com/viewer/postView.nhn?volumeNo=8292571&memberNo=33582594"
+            intro="The benefits of using monolingual flashcards"
+            date="2017-06-24"
+          />
+          <Listing
+            name="Introducing React Refetch"
+            href="https://blog.heroku.com/react-refetch"
+            code="https://github.com/heroku/react-refetch"
+            intro="A simple, declarative, and composable way to fetch data for React components"
+            date="2015-12-15"
+          />
+          <Listing
+            name="Bulk API Queries"
+            href="https://developer.salesforce.com/blogs/engineering/2011/03/bulk-api-queries.html"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="Bulk query support in Workbench 3.0"
+            date="2011-03-21"
+          />
+          <Listing
+            name="Workbench for Winter '11 Now Available"
+            href="https://developer.salesforce.com/blogs/engineering/2010/10/workbench-for-winter-11-now-available.html"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="Workbench enhancements for Winter '11 Release"
+            date="2010-10-17"
+          />
+          <Listing
+            name="Workbench 3.0 with Metadata API Support and More!"
+            href="https://developer.salesforce.com/blogs/engineering/2010/06/workbench-30-with-metadata-api-support-and-more.html"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="Enhancement for Workbench 3.0"
+            date="2010-06-20"
+          />
+          <Listing
+            name="PHP Client for Force.com Bulk API"
+            href="https://developer.salesforce.com/blogs/engineering/2009/11/php-client-for-forcecom-bulk-api.html"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="Introduction of PHP client for Bulk API"
+            date="2009-11-07"
+          />
+          <Listing
+            name="Workbench: From Idea to 2.0"
+            href="https://developer.salesforce.com/blogs/engineering/2008/07/workbench-from.html"
+            code="https://github.com/ryanbrainard/forceworkbench"
+            intro="How Workbench went from a modest idea to shipping v2.0"
+            date="2008-07-01"
+          />
+        </ListingGroup>
       </div>
     );
   }
@@ -136,6 +131,15 @@ function HeadlineIcon({ fa, href }) {
         <FA name={fa} />
       </a>
     </li>
+  );
+}
+
+function ListingGroup({ name, children }) {
+  return (
+    <div style={{ float: "left" }}>
+      <h2>{name}</h2>
+      <ul>{children}</ul>
+    </div>
   );
 }
 
